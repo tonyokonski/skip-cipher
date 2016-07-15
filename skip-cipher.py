00e3a3c78f6c927a23a98eeca2098f844e82e0bc
Tony <tonyokonski@gmail.com>
2016-07-15 15:50:44 -0500

    added exception handling, separate key function for DRY, split old main() into logical functions

--- skip-cipher.py
+++ skip-cipher.py
@@ -1,6 +1,6 @@
 # performs a skip cypher. Assume perfect input; no error handling yet. 
 
-import random  
+import random, getpass  
 
 def scramble():
 
@@ -22,6 +22,18 @@ def random_characters(number):
 
     return new_string
 
+def key_gen():
+    try:
+        main_key = int(getpass.getpass('Enter a positive integer as a numerical key: ')) #need try except here
+    except ValueError as err:
+        print err, '; please enter a positive integer.'
+        key_gen()
+    else:
+        if main_key < 1:
+            print 'Please enter a positive integer.'
+            key_gen()
+        else:
+            return main_key
 
 def encrypt(msg, key):
     #msg is the string to encrypt and key is the cryptographic key
@@ -36,12 +48,20 @@ def encrypt(msg, key):
         encrypted_msg += (encrypt_cypher + msg[i])
         i += 1
         
-    #print encrypt_cypher # for testing purposes
-    #print 'Encrypted message is:', encrypted_msg  # for testing purposes
-        
-    return encrypted_msg + encrypt_tail
+    encrypted =  encrypted_msg + encrypt_tail
+   
+    print 'Encrypted message is:', encrypted
 
+def encrypt_prep():
+    
+    main_msg = raw_input('Please type in the message that you would like to encrypt and then press ENTER: ')
+    
+    new_main_msg = ''.join(main_msg.split())
+    
+    key = key_gen()
 
+    encrypted_message = encrypt(new_main_msg, key)
+    
 def decrypt(msg, key):
     # msg is the string to decrypt and key is the cryptographic key    
     
@@ -58,56 +78,21 @@ def decrypt(msg, key):
     
     return decrypted_msg
 
-def main():
-    # drives the program
-    
-    choice = int(raw_input('Encrypt (1) or Decrypt (2): '))
-    main_msg = raw_input('Message: ')
-    new_main_msg = ''
-    main_key = int(raw_input('Key: '))
-    i = 0   
-    
-    if choice == 1: # if spaces are in the string, removes spaces and sends string to encrypt function. Else sends string to encrypt function.
-        while i < len(main_msg): # need to account for main_msg[-1] == ' '
-            if main_msg[i] == ' ':
-                i += 1
-                new_main_msg += main_msg[i]
-            else:
-                new_main_msg += main_msg[i]
-                
-            i += 1
-            
-            encrypted_message = encrypt(new_main_msg, main_key)
-            
-        #print new_main_msg # for testing purposes        
+def secrecy():
 
-        print # prints an empty line
-        print 'Encrypted message:', encrypted_message        
+    try:
+        x = int(raw_input('Encrypt (1) OR decrypt (2): '))        
+    except ValueError as err:
+        print err, '; please enter a 1 to encrypt or a 2 to decrypt.'
+        secrecy()
     else:
-        decrypted_message = decrypt(main_msg,main_key) # as per instructions, the cypher to decrypt will not have spaces. User provides the random values * key between each letter.
-        
-        print # prints an empty line
-        print 'Decrypted message:', decrypted_message
-        
-main()
-
-# Prep for re-doing the initial function
-
-#def secrecy():
-#
-#    try:
-#        x = int(raw_input('Encrypt (1) OR decrypt (2): '))        
-#    except ValueError as err:
-#        print err, '; please enter a 1 to encrypt or a 2 to decrypt.'
-#        secrecy()
-#    else:
-#        if x == 1:
-#            print 'Sent to encryption funcion'
-#        elif x == 2:
-#            print 'Sent to decryption function'
-#        else:
-#            print 'Number entered is not a 1 or 2. Please enter a 1 to encrypt or a 2 to decrypt.'
-#            secrecy()
-#                    
-#if __name__ == '__main__':
-#    secrecy()
+        if x == 1:
+            encrypt_prep()
+        elif x == 2:
+            print 'Sent to decryption function'
+        else:
+            print 'Number entered is not a 1 or 2. Please enter a 1 to encrypt or a 2 to decrypt.'
+            secrecy()
+                    
+if __name__ == '__main__':
+    secrecy()
